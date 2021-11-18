db0d6300b17eb899bb58b78fce4c836110040996
gurvir93 <gur_vir_30@hotmail.com>
2021-11-17 19:56:46 -0700

    Add project files.

---
+++ src/Domain/Entity/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GroceryCo.Domain
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public string UPC { get; set; }
+        public decimal Price { get; set; } = 0.0m;
+        public bool IsDiscounted { get; set; } = false;
+        public int DiscountPercent { get; set; } = 0;
+    }
+}
